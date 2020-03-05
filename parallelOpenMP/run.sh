23459094d8a9887cb3120383d10e4b3b2d132f98
Archund <jarchund@gmail.com>
2020-03-05 03:57:39 -0600

    Adds OpenMP implementation
    __needs testing__

---
+++ parallelOpenMP/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE_SOURCE="img/originals"
+IMAGE_RESULTS="img/results/OpenMP"
+
+smallImageTime=$(./convImage "$IMAGE_SOURCE/imageSmall.jpg" "$IMAGE_RESULTS/resultSmall.jpg")
+echo -ne "Convolution for smallImage using the OpenMP algorithm took $smallImageTime seconds\n"
+
+normalImageTime=$(./convImage "$IMAGE_SOURCE/imageNormal.jpg" "$IMAGE_RESULTS/resultNormal.jpg")
+echo -ne "Convolution for normalImage using the OpenMP algorithm took $normalImageTime seconds\n"
+
+largeImageTime=$(./convImage "$IMAGE_SOURCE/imageBig.jpg" "$IMAGE_RESULTS/resultBig.jpg")
+echo -ne "Convolution for largeImage using the OpenMP algorithm took $largeImageTime seconds\n"
